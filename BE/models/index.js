caebbd54fa0bf228ed8de21c1ec8224e1e8b063b
nguyentiendat09 <luanoutplay@gmail.com>
2025-02-20 23:44:44 +0700

    add Notification model and update model imports to include Sequelize

--- BE/models/index.js
+++ BE/models/index.js
@@ -3,29 +3,35 @@ const config = require("../db/config.js");
 
 // Khởi tạo Sequelize với cấu hình từ `config.js`
 const sequelize = new Sequelize(config.database, config.user, config.password, {
-    host: config.host,
-    dialect: "mysql",
-    dialectModule: require("mysql2"),
-    logging: false, // Tắt log query SQL
+  host: config.host,
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
+  logging: false, // Tắt log query SQL
 });
 
 // Kiểm tra kết nối
 sequelize
-    .authenticate()
-    .then(() => console.log("Connected to MySQL successfully"))
-    .catch((error) => console.error("Unable to connect to the database:", error));
+  .authenticate()
+  .then(() => console.log("Connected to MySQL successfully"))
+  .catch((error) => console.error("Unable to connect to the database:", error));
 
 // Import các model
 const Tour = require("./tour.model")(sequelize, Sequelize);
-const User = require("./user.model")(sequelize);
-const Customer = require("./customer.model")(sequelize);
-const Booking = require("./booking.model")(sequelize);
-const DiscountService = require("./discountService.model.js")(sequelize);
-const Feedback = require("./feedback.model")(sequelize);
-const GuideTour = require("./guideTour.model.js")(sequelize);
-const Location = require("./location.model.js")(sequelize);
-const Notification = require("./notification.model.js")(sequelize);
-const NotificationType = require("./notificationType.model.js")(sequelize);
+const User = require("./user.model")(sequelize, Sequelize);
+const Customer = require("./customer.model")(sequelize, Sequelize);
+const Booking = require("./booking.model")(sequelize, Sequelize);
+const DiscountService = require("./discountService.model.js")(
+  sequelize,
+  Sequelize
+);
+const Feedback = require("./feedback.model")(sequelize, Sequelize);
+const GuideTour = require("./guideTour.model.js")(sequelize, Sequelize);
+const Location = require("./location.model.js")(sequelize, Sequelize);
+const Notification = require("./notification.model.js")(sequelize, Sequelize);
+const NotificationType = require("./notificationType.model.js")(
+  sequelize,
+  Sequelize
+);
 const Service = require("./service.model")(sequelize, Sequelize);
 const TourService = require("./tour_service.model")(sequelize, Sequelize);
 
