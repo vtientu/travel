05ac12ebdd6fe7ec11c69beebc242421febb7832
Michael Dang <94636348+dmcgithub315@users.noreply.github.com>
2025-03-10 13:49:19 +0700

    -cuongdm update

---
+++ BE/models/voucher.model.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, Sequelize) => {
+  const Voucher = sequelize.define(
+    "Voucher",
+    {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      voucher_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      discount_percentage: {
+        type: Sequelize.INTEGER,
+      },
+      discount_amount: {
+        type: Sequelize.INTEGER,
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+      },
+    },
+    {
+      tableName: "voucher", // Tên bảng trong MySQL
+      timestamps: false, // Không dùng createdAt và updatedAt
+    }
+  );
+
+  return Voucher;
+};
