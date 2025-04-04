f4166a3cbff6446d6306765d53b4ccd7ba0d294c
Michael Dang <94636348+dmcgithub315@users.noreply.github.com>
2025-04-04 10:52:34 +0700

    -cuongdm update

--- BE/routes/payment.route.js
+++ BE/routes/payment.route.js
@@ -4,7 +4,7 @@ const paymentController = require("../controllers/payment.controller");
 
 router.post("/check", paymentController.checkPayment);
 router.get("/", paymentController.getAllPayment);
-router.get("/:id", paymentController.getPaymentById);
+router.get("/:id", paymentController.getPayment);
 router.get("/booking/:id", paymentController.getPaymentByBookingId);
 router.get("/customer/:id", paymentController.getPaymentByCustomerId);
 
